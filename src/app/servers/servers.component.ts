bd18335afee0c7597c434252b947e6e63fcc7352
rtimelston <13246079+rtimelston@users.noreply.github.com>
2021-06-07 15:21:40 -0700

    Chapter 2 - Understanding the Component Selector

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+//   selector: 'app-servers', // select by element; this is preferred
+//   selector: '[app-servers]', // select by attribute
+  selector: '.app-servers', // select by class
+  // Note: selection by id and pseudo attributes is not supported
   template: `
   <app-server></app-server>
   <app-server></app-server>
